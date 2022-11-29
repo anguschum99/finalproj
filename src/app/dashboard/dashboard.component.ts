16f2fe27737a2dfdf56455e67368d7307bfe9463
spoooonks <syracusebear@gmail.com>
2022-11-29 13:54:54 -0500

    Third Commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -18,6 +18,6 @@ export class DashboardComponent implements OnInit {
 
   getItems(): void {
     this.itemService.getItems()
-      .subscribe(items => this.items = items.slice(1, 5));
+      .subscribe(items => this.items = items.slice(1, 10));
   }
 }
