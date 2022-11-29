3104c04a6abf06f75b877bed4543d63d2f592f4a
spoooonks <syracusebear@gmail.com>
2022-11-28 22:34:12 -0500

    first commit

---
+++ src/app/mock-items.ts
@@ -0,0 +1,14 @@
+import { Item } from './item';
+
+export const ITEMS: Item[] = [
+  { id: 12, name: 'Fish' },
+  { id: 13, name: 'Chicken' },
+  { id: 14, name: 'Broccoli' },
+  { id: 15, name: 'Tomato' },
+  { id: 16, name: 'Orange' },
+  { id: 17, name: 'Leek' },
+  { id: 18, name: 'Water' },
+  { id: 19, name: 'Pencils' },
+  { id: 20, name: 'Paper' },
+  { id: 21, name: 'Watermelon' }
+];
