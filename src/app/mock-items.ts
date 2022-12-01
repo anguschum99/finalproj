90d6051ffadf4e5529f67d3076f318140ceba31d
Ohm-sudo <45316019+Ohm-sudo@users.noreply.github.com>
2022-11-30 19:22:29 -0500

    Update mock-items.ts

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -1,14 +1,14 @@
 import { Item } from './item';
 
 export const ITEMS: Item[] = [
-  { id: 1, name: 'Fish', desc:'Delicious Salmon'},
-  { id: 2, name: 'Chicken', desc:'Raw Chicken' },
-  { id: 3, name: 'Broccoli', desc:'Fresh Broccoli' },
-  { id: 4, name: 'Tomato', desc: 'Tomato' },
-  { id: 5, name: 'Orange', desc:'Fresh Picked Oranges' },
-  { id: 6, name: 'Leek', desc:'leek' },
-  { id: 7, name: 'Water',desc:'Dasani' },
-  { id: 8, name: 'Pencils',desc:'0.7 lead' },
-  { id: 9, name: 'Paper', desc:'White Paper' },
-  { id: 10, name: 'Watermelon', desc:'Juicy Watermelon' }
+  { id: 1, name: 'Fish', desc:'Delicious Salmon', img: 'assets/fish.jpg'},
+  { id: 2, name: 'Chicken', desc:'Raw Chicken', img: 'assets/chicken.jpg'},
+  { id: 3, name: 'Broccoli', desc:'Fresh Broccoli', img: 'assets/broccoli.jpg' },
+  { id: 4, name: 'Tomato', desc: 'Tomato', img: 'assets/tomato.jpg' },
+  { id: 5, name: 'Orange', desc:'Fresh Picked Oranges', img: 'assets/orange.jpg' },
+  { id: 6, name: 'Leek', desc:'leek', img:'assets/leek.jpg' },
+  { id: 7, name: 'Water',desc:'Dasani', img:'assets/water.jpg' },
+  { id: 8, name: 'Pencils',desc:'0.7 lead', img:'assets/pencils.jpg' },
+  { id: 9, name: 'Paper', desc:'White Paper', img:'assets/paper.jpg' },
+  { id: 10, name: 'Watermelon', desc:'Juicy Watermelon', img:'assets/watermelon.jpg'}
 ];
