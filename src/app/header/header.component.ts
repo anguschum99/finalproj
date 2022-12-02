2089be5cd13e87fd48a5ddcada022426432ad50a
Ohm-sudo <45316019+Ohm-sudo@users.noreply.github.com>
2022-12-02 12:20:46 -0500

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  currentDate = new Date();
-  timestamp = this.currentDate.toLocaleString('en-US', 
-  {month:'short', day:'2-digit', 
-  year:'numeric', hour:'numeric', minute:'numeric'});
+export class HeaderComponent implements OnInit {
+  
+  time = new Date();
+  intervalId: NodeJS.Timer;
 
+  ngOnInit(){
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+  }
 }
